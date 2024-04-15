bc841a30027997b679273760034679bd68bea100
Francerz <francerzam@hotmail.com>
2024-04-15 13:35:16 -0600

    2024-04-12

---
+++ app/screens/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+function Home(): React.JSX.Element {
+  
+  return (
+    <SafeAreaView>
+      <Text>Hola</Text>
+    </SafeAreaView>
+  );
+}
+
+export default Home;
