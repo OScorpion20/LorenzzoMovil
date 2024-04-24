198ac59a6d6445f4bde08be6b58d896ac4aae655
Francerz <francerzam@hotmail.com>
2024-04-24 13:15:20 -0600

    2024-04-22

--- app/screens/Login.tsx
+++ app/screens/Login.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import {StackNavigationProp} from '@react-navigation/stack';
+import React, {useState} from 'react';
 import {
+  Alert,
   Button,
   SafeAreaView,
   StyleSheet,
@@ -9,9 +11,18 @@ import {
 } from 'react-native';
 
 const styles = StyleSheet.create({
+  screen: {
+    height: '100%',
+    backgroundColor: '#323844',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#c0c0c040',
+    width: '100%',
+    padding: 16,
   },
   textInput: {
     borderBottomWidth: 1,
@@ -24,18 +35,43 @@ const styles = StyleSheet.create({
   },
 });
 
-function Login(): React.JSX.Element {
+type RootStackParamList = {
+  Home: undefined;
+  Login: undefined;
+};
+type LoginProps = {
+  navigation: StackNavigationProp<RootStackParamList, 'Home'>;
+};
+
+function Login({navigation}: LoginProps): React.JSX.Element {
+  const [usuario, setUsuario] = useState('');
+  const [contrasena, setContrasena] = useState('');
+
+  const btnIngresarOnPress = async function () {
+    if (usuario && contrasena) {
+      navigation.navigate('Home');
+      return;
+    }
+    Alert.alert('Fallido', 'Datos incorrectos');
+  };
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.screen}>
       <View style={styles.container}>
         <Text>Iniciar sesión</Text>
-        <TextInput style={styles.textInput} placeholder="Usuario" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Usuario"
+          placeholderTextColor="#828894"
+          onChangeText={u => setUsuario(u)}
+        />
         <TextInput
           style={styles.textInput}
           placeholder="Contraseña"
+          placeholderTextColor="#828894"
           secureTextEntry={true}
+          onChangeText={p => setContrasena(p)}
         />
-        <Button title="Ingresar" />
+        <Button title="Ingresar" onPress={btnIngresarOnPress} />
       </View>
     </SafeAreaView>
   );
